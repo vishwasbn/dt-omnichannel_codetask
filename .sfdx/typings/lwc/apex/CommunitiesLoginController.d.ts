e06570577f1da534fcd7a35161c6cb019027f481
vishwasbn <53083705+vishwasbn@users.noreply.github.com>
2023-03-20 17:53:52 +0530

    - apex initial code

---
+++ .sfdx/typings/lwc/apex/CommunitiesLoginController.d.ts
@@ -0,0 +1,9 @@
+declare module "@salesforce/apex/CommunitiesLoginController.loginUser" {
+  export default function loginUser(param: {username: any, password: any, urlParam: any}): Promise<any>;
+}
+declare module "@salesforce/apex/CommunitiesLoginController.loginUser" {
+  export default function loginUser(param: {username: any, password: any, urlParam: any, redirect: any}): Promise<any>;
+}
+declare module "@salesforce/apex/CommunitiesLoginController.resetUserPassword" {
+  export default function resetUserPassword(param: {username: any}): Promise<any>;
+}
---
+++ .sfdx/typings/lwc/apex/ContactPortalInviteIdentifierController.d.ts
@@ -0,0 +1,6 @@
+declare module "@salesforce/apex/ContactPortalInviteIdentifierController.checkPortalUser" {
+  export default function checkPortalUser(param: {contactId: any}): Promise<any>;
+}
+declare module "@salesforce/apex/ContactPortalInviteIdentifierController.checkPermissions" {
+  export default function checkPermissions(param: {contactId: any}): Promise<any>;
+}
