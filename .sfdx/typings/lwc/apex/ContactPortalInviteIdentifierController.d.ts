8572a5f7050698308e5ed715ef4a66daf4400492
vishwasbn <53083705+vishwasbn@users.noreply.github.com>
2023-06-29 15:22:36 +0530

    - vscode file

--- .sfdx/typings/lwc/apex/ContactPortalInviteIdentifierController.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "@salesforce/apex/ContactPortalInviteIdentifierController.checkPortalUser" {
-  export default function checkPortalUser(param: {contactId: any}): Promise<any>;
-}
-declare module "@salesforce/apex/ContactPortalInviteIdentifierController.checkPermissions" {
-  export default function checkPermissions(param: {contactId: any}): Promise<any>;
-}
