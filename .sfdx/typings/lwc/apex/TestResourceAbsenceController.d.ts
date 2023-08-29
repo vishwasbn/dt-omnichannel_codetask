577a5acbfcbfded8f7e07782b4572f6d1018de47
vishwasbn <53083705+vishwasbn@users.noreply.github.com>
2023-08-29 20:50:21 +0530

    - bkp of initial absence creation lwc code

---
+++ .sfdx/typings/lwc/apex/TestResourceAbsenceController.d.ts
@@ -0,0 +1,6 @@
+declare module "@salesforce/apex/TestResourceAbsenceController.getAllStores" {
+  export default function getAllStores(param: {searchKey: any}): Promise<any>;
+}
+declare module "@salesforce/apex/TestResourceAbsenceController.createAbsenceRecords" {
+  export default function createAbsenceRecords(param: {requestData: any}): Promise<any>;
+}
